3160c1d406603e2347f7937184a12ec241947160
Anton Stroy <antosha.stroy@gmail.com>
2020-10-14 01:24:01 -0500

    Added validation and associations to operato and subtype

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,2 +1,6 @@
 class Operator < ApplicationRecord
+  has_many :aircraft_operators
+  has_many :aircrafts, through: :aircraft_operators
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/subtype.rb
+++ app/models/subtype.rb
@@ -1,2 +1,6 @@
 class Subtype < ApplicationRecord
+  has_many :aircraft_subtypes
+  has_many :aircrafts, through: :aircraft_subtypes
+
+  validates :name, presence: true, uniqueness: true
 end
