56ea5b62cf2215bf88d722aaf692140071ea32f4
Anton Stroy <antosha.stroy@gmail.com>
2020-10-21 17:40:35 -0500

    created view for aircraft and countries pages

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -1,7 +1,9 @@
 class AircraftsController < ApplicationController
   def index
+    @aircrafts = Aircraft.includes(:origin_country).order("name")
   end
 
   def show
+    @aircraft = Aircraft.find(params[:id])
   end
 end
--- app/controllers/origin_countries_controller.rb
+++ app/controllers/origin_countries_controller.rb
@@ -1,7 +1,9 @@
 class OriginCountriesController < ApplicationController
   def index
+    @origin_countries = OriginCountry.all
   end
 
   def show
+    @origin_countries = OriginCountry.find(params[:id])
   end
 end
