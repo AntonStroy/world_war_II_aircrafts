3ad243c50673b631b6c65b228a39f4dadaf8eb62
Anton Stroy <antosha.stroy@gmail.com>
2020-10-22 00:21:40 -0500

    created a home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :aircrafts, only: %i[index show]
   resources :origin_countries, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
