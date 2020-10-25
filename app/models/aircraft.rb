d8638482d00744e0ed2558df73f95f5af1607ed8
Anton Stroy <antosha.stroy@gmail.com>
2020-10-25 16:39:53 -0500

    Added the subtype data to the aircraft view

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -6,4 +6,8 @@ class Aircraft < ApplicationRecord
   validates :name, :aircraft_type, presence: true
   validates :service_start, :units_build, numericality: { only_integer: true }
   validates :unit_price, numericality: { greater_than_or_equal_to: 0 }
+
+  def subtype_list
+    subtypes.map(&:name).join(", ")
+  end
 end
